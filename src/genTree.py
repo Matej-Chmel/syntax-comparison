3f3daf6dbab5e958f952f1a40f6a67b72d635bbc
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2021-08-06 22:57:27 +0200

    New task: Complete tree. Python scripts now use ArgumentParser. Fix: Snippet metadata generated for every language.

--- src/genTree.py
+++ src/genTree.py
@@ -1,36 +1,71 @@
+from argparse import ArgumentParser
 from json import dump
+from functools import partial
 from pathlib import Path
-from sys import argv, exit
+from sys import exit
 from typing import Callable
+
+class AppError(Exception):
+	def __init__(self, msg: str):
+		super().__init__(msg)
+
+def addCompleteArg(parser: ArgumentParser):
+	group = parser.add_mutually_exclusive_group(required=True)
+	group.add_argument(
+		"-c", "--complete", action="store_true", help="Complete existing "
+		f"folder structure. {mutExHelp('gen')}")
+	group.add_argument(
+		"-g", "--gen", action="store_false", help="Generate folder structure "
+		f"for new snippet. {mutExHelp('complete')}")
+
+def args():
+	parser = ArgumentParser(
+		prog="GEN_TREE",
+		description="Generates folder structure for new snippet or "
+		"completes existing folder structure with missing languages.")
+
+	addCompleteArg(parser)
+	parser.add_argument(
+		"nameOrPath",
+		help="With --complete pass a path to any file or folder in the tree of "
+		"a snippet to complete. With --gen pass a name for the new snippet.")
+	res = parser.parse_args()
+
+	if res.complete:
+		return True, Path(res.nameOrPath).absolute()
+	return False, str(res.nameOrPath)
 
 def camelCase(split: list[str]):
 	return "".join([
-		fmtFirstWord(split[0], str.lower),
-		*map(str.title, split[1:])])
+		fmtNotUpper(split[0], str.lower),
+		*map(partial(fmtNotUpper, effect=str.title), split[1:])])
 
 def capitalize(split: list[str]):
-	return " ".join([fmtFirstWord(split[0], str.title), *split[1:]])
+	return " ".join([fmtNotUpper(split[0], str.title), *split[1:]])
 
-def fmtFirstWord(s: str, effect: Callable[[str], str]):
+def fmtNotUpper(s: str, effect: Callable[[str], str]):
 	return s if s.isupper() else effect(s)
 
 def fopen(p: Path, mode: str = "w"):
 	return p.open(mode, encoding="utf-8")
 
 def genLangTree(progName: str, snipDir: Path, template: Path):
-	ext = template.suffix.lower().replace(".", "")
 	langDir = snipDir / template.parent.name
-
-	genMetadata(snipDir)
-	langDir.mkdir()
+	safeMkdir(langDir)
+	ext = template.suffix.lower().replace(".", "")
 
 	with fopen(langDir / "README.md") as f:
 		f.write(mdShort(ext))
-	with fopen(langDir / f'{progName}.{ext}') as f, fopen(template, "r") as t:
+	with fopen(langDir / f"{progName}.{ext}") as f, fopen(template, "r") as t:
 		f.write(t.read())
 
 def genMetadata(snipDir: Path):
-	with fopen(snipDir / "metadata.json") as f:
+	p = snipDir / "metadata.json"
+
+	if p.exists():
+		raise AppError(f"Metadata for \"{snipDir.name}\" already exist.")
+
+	with fopen(p) as f:
 		dump({"aliases": []}, f, indent=4, sort_keys=True)
 		f.write("\n")
 
@@ -39,30 +74,40 @@ def mdShort(ext: str):
 		highlight = mdSyntaxHighlight[ext]
 	except KeyError:
 		highlight = ext
-	return f'```{highlight}{NEWLINE * 2}```{NEWLINE}'
+	return f"```{highlight}{NEWLINE * 2}```{NEWLINE}"
 
 mdSyntaxHighlight = {
 	"py": "python"
 }
-NEWLINE = '\n'
 
-def safeMkdir(snipDir: Path):
+def mutExHelp(otherArg: str):
+	return f"Cannot be used with --{otherArg} argument."
+
+NEWLINE = "\n"
+
+def safeMkdir(p: Path):
 	try:
-		snipDir.mkdir()
+		p.mkdir()
 
 	except Exception:
-		for _ in snipDir.iterdir():
-			raise RuntimeError(
-				f"\"{snipDir.name}\" directory already exists and isn't empty.")
+		for _ in p.iterdir():
+			raise AppError(
+				f"\"{p.name}\" directory already exists and isn't empty.")
 
 def snipDir(name: str, src: Path):
-	return src / "snippets" / name
+	return snippetsDir(src) / name
 
-def snipNameArg():
-	try:
-		return argv[1].strip()
-	except IndexError:
-		raise RuntimeError("No snippet name given.")
+def snipDirFromPath(p: Path, src: Path):
+	snippets = snippetsDir(src)
+
+	if not p.is_relative_to(snippets):
+		raise AppError(
+			f"\"{p.name}\" doesn't reside inside a snippet directory.")
+
+	return snippets / p.relative_to(snippets).parts[0]
+
+def snippetsDir(src: Path):
+	return src / "snippets"
 
 def srcDir():
 	return Path(__file__).parent
@@ -75,17 +120,32 @@ def templateFiles(templateDir: Path):
 
 def main():
 	try:
+		complete, nameOrPath = args()
 		src = srcDir()
-		snipNameSplit = snipNameArg().split()
-		snip = snipDir(capitalize(snipNameSplit), src)
 
-		safeMkdir(snip)
-		progName = camelCase(snipNameSplit)
+		if complete:
+			snip = snipDirFromPath(nameOrPath, src)
+			progName = camelCase(snip.name.split())
+		else:
+			snipNameSplit = nameOrPath.split()
+			snip = snipDir(capitalize(snipNameSplit), src)
+			progName = camelCase(snipNameSplit)
+			safeMkdir(snip)
+
+		try:
+			genMetadata(snip)
+			print(f"Generated metadata for \"{snip.name}\".")
+		except AppError as e:
+			print(e)
 
 		for t in templateFiles(templateDir(src)):
-			genLangTree(progName, snip, t)
+			try:
+				genLangTree(progName, snip, t)
+				print(f"Generated tree for {t.parent.name}.")
+			except AppError as e:
+				print(e)
 
-	except RuntimeError as e:
+	except AppError as e:
 		print(e)
 		exit(1)
 
--- src/run.py
+++ src/run.py
@@ -1,8 +1,28 @@
+from argparse import ArgumentParser
 from pathlib import Path
 from platform import system
 from subprocess import call
-from sys import argv, exit
+from sys import exit
 from typing import Callable
+
+class AppError(Exception):
+	def __init__(self, msg: str):
+		super().__init__(msg)
+
+def args():
+	parser = ArgumentParser(
+		prog="RUNNER",
+		description="Recognizes language and runs supplied program. "
+		"Cache in \"src/out\" directory is used for repeated runs.")
+
+	parser.add_argument("path", help="Path to one of the source files.")
+	parser.add_argument(
+		"-r", "--rebuild", action="store_true",
+		help="Force recompilation of the program. "
+		"Doesn't have any effect for interpreted languages without cache.")
+	res = parser.parse_args()
+
+	return Path(res.path).absolute(), res.rebuild
 
 def buildNeeded(caller: Path, outFile: Path, rebuild: bool):
 	return (
@@ -10,31 +30,18 @@ def buildNeeded(caller: Path, outFile: Path, rebuild: bool):
 		not outFile.exists() or
 		outFile.stat().st_mtime < caller.stat().st_mtime)
 
-def callerPath():
-	try:
-		return Path(argv[1]).absolute()
-	except IndexError:
-		raise RuntimeError("Path to program is missing.")
-
 def callProg(args: list, **kwargs):
 	return call(map(str, args), **kwargs)
 
 def checkBuild(outFile: Path):
 	if not outFile.exists():
-		raise RuntimeError(
-			f"Compilation failed, {outFile.name} wasn't created.")
+		raise AppError(f"Compilation failed, {outFile.name} wasn't created.")
 
 def outDirPath():
 	return Path(__file__).parent / "out"
 
 def pythonCmd():
 	return "python" if system().lower().startswith("linux") else "py"
-
-def rebuildOpt():
-	try:
-		return argv[2].lower() == "true"
-	except IndexError:
-		return False
 
 RunFunc = Callable[[Path, Path, bool], None]
 runners: dict[str, RunFunc] = {}
@@ -63,19 +70,19 @@ def runPython(caller: Path, *_):
 
 def main():
 	try:
-		caller = callerPath()
+		caller, rebuild = args()
 		ext = caller.suffix.replace(".", "").lower()
 		out = outDirPath()
+
 		out.mkdir(exist_ok=True)
-		rebuild = rebuildOpt()
 		runners[ext](caller, out, rebuild)
+
+	except AppError as e:
+		print(e)
+		exit(1)
 
 	except KeyError as e:
 		print(f"Unsupported extension: {e.args[0]}")
-		exit(1)
-
-	except RuntimeError as e:
-		print(e)
 		exit(1)
 
 if __name__ == "__main__":
